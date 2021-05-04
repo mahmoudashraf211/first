ab83cd26b0d9e30ee9c75667bb73667148dd6c5d
Mahmoud ashraf <mahmoudashraf211>
2021-05-04 11:20:20 +0200

    third

--- Functionality.js
+++ Functionality.js
@@ -1,95 +1,116 @@
 // mathmatical equations
-function get_Volt_By_Power_And_Current (p , i ){
-    return p/i;
-}
-function get_Volt_By_Resistance_And_Current (r , i ){
-    return i*r;
-}
-function get_Volt_By_Power_And_Resistance (p , r ){
-    return Math.sqrt(p*r);
-}
-
-
-function get_Current_By_Power_And_Resistance (p,r){
-    return Math.sqrt(p/r);
-}
-function get_Current_By_Power_And_Voltage (p,v){
-    return p/v;
-}
-function get_Current_By_Volt_And_Resistance (v,r){
-    return v/r;
-}
-
-
-function get_Power_By_Volt_And_Current(v,i){
-    return v*i;
-}
-function get_Power_By_Volt_And_Resistance(v,r){
-    return Math.pow(v,2)/r;
-}
-function get_Power_By_Current_And_Resistance(i,r){
-    return Math.pow(i,2)*r;
+function Equations(){
+    if (Get_Menu.value ==="Resistance" && First_Given.innerHTML === "Volt" && Second_Given.innerHTML==="Current")
+        Result.innerHTML="Result ... " +parseInt(Input1.value,10)/parseInt(Input2.value,10);
+    else if (Get_Menu.value ==="Power" && First_Given.innerHTML === "Volt" && Second_Given.innerHTML==="Current")
+        Result.innerHTML="Result ... " +parseInt(Input1.value,10)*parseInt(Input2.value,10);
+    else if (Get_Menu.value === "Resistance" && First_Given.innerHTML === "Power" && Second_Given.innerHTML==="Current")
+        Result.innerHTML="Result ... " +parseInt(Input1.value,10)/Math.pow(parseInt(Input2.value,10),2);
+    else if (Get_Menu.value === "Voltage" && First_Given.innerHTML === "Power" && Second_Given.innerHTML==="Current")
+        Result.innerHTML="Result ... " +parseInt(Input1.value,10)/parseInt(Input2.value,10);
+    else if (Get_Menu.value === "Resistance" && First_Given.innerHTML === "Power" && Second_Given.innerHTML==="Volt")
+        Result.innerHTML="Result ... " +Math.pow(parseInt(Input2.value,10),2)/parseInt(Input1.value,10);
+    else if (Get_Menu.value === "Voltage" && First_Given.innerHTML === "Power" && Second_Given.innerHTML==="Resistance")
+        Result.innerHTML="Result ... "+Math.sqrt(parseInt(Input1.value,10)*parseInt(Input2.value,10));
+    else if (Get_Menu.value === "Voltage" && First_Given.innerHTML === "Current" && Second_Given.innerHTML==="Resistance")
+        Result.innerHTML="Result ... " +parseInt(Input1.value,10)*parseInt(Input2.value,10);
+    else if (Get_Menu.value ==="Power" && First_Given.innerHTML === "Volt" && Second_Given.innerHTML==="Resistance")
+        Result.innerHTML="Result ... " +Math.pow(parseInt(Input1.value,10),2)/parseInt(Input2.value,10);
+    else if (Get_Menu.value ==="Power" && First_Given.innerHTML === "Current" && Second_Given.innerHTML==="Resistance")
+        Result.innerHTML="Result ... " +Math.pow(parseInt(Input1.value,10),2)*parseInt(Input2.value,10);
+    else if (Get_Menu.value ==="Current" && First_Given.innerHTML === "Power" && Second_Given.innerHTML==="Resistance")
+        Result.innerHTML="Result ... "+Math.sqrt(parseInt(Input1.value,10)/parseInt(Input2.value,10));
+    else if (Get_Menu.value ==="Current" && First_Given.innerHTML === "Power" && Second_Given.innerHTML==="Volt")
+        Result.innerHTML="Result ... " +parseInt(Input1.value,10)/parseInt(Input2.value,10);
+    else if (Get_Menu.value ==="Current" && First_Given.innerHTML === "Volt" && Second_Given.innerHTML==="Resistance")
+        Result.innerHTML="Result ... " +parseInt(Input1.value,10)/parseInt(Input2.value,10);
+    else
+    alert("Nope ! not working ");    
 }
 
 
-function get_Resistance_By_Volt_And_Current(v,i){
-    return v/i;
+// this funciton made to update the input lables according to the list of givens (by ..)
+function changeBackGround(){
+
+if (menu.value===First_Of_By.value && First_Of_By.innerHTML==="Power And Resistance"){
+    First_Given.innerHTML="Power";
+    Second_Given.innerHTML="Resistance";
+}else if (menu.value===First_Of_By.value && First_Of_By.innerHTML==="Volt And Current"){
+    First_Given.innerHTML="Volt";
+    Second_Given.innerHTML="Current";
+}else if (menu.value===Second_Of_By.value && Second_Of_By.innerHTML==="Power And Current"){
+    First_Given.innerHTML="Power";
+    Second_Given.innerHTML="Current";
+}else if (menu.value===Second_Of_By.value && Second_Of_By.innerHTML==="Volt And Resistance"){
+    First_Given.innerHTML="Volt";
+    Second_Given.innerHTML="Resistance";
+}else if (menu.value===Third_Of_By.value && Third_Of_By.innerHTML==="Current And Resistance"){
+    First_Given.innerHTML="Current";
+    Second_Given.innerHTML="Resistance";
+}else if (menu.value===Third_Of_By.value && Third_Of_By.innerHTML==="Power And Volt"){
+    First_Given.innerHTML="Power";
+    Second_Given.innerHTML="Volt";
+}else {
+    First_Given.innerHTML="???????????";
+    Second_Given.innerHTML="???????????";
 }
-function get_Resistance_By_Volt_And_Power(v,p){
-    return Math.pow(v,2)/p;
-}
-function get_Resistance_By_Current_And_Power(i,p){
-    return p/Math.pow(i,2);
 }
 
-// supposed to be the labels chaning functions .. when the user for example chooses power and current so the label of 
-// the first input is power and the second is current and so on
-
-function Changing_Labels1(event){
-    First_Given.innerHTML("Voltage");
-    Second_Given.innerHTML("Current");
-} 
-function Changing_Labels2(event){
-    First_Given.innerHTML("Voltage");
-    Second_Given.innerHTML("Power");
-}
-function Changing_Labels3(event){
-    First_Given.innerHTML("Power");
-    Second_Given.innerHTML("Current");
-}
-
-// this funciton was a trial for accessing the menu elements .. and it worked but for somehow the event works only once.
-
-function changeBackGround(m){
-    if(m.value === voltage_And_Current){
-    alert("Voltage and Current");
-    }else if (m.value === Voltage_And_Power){
-    alert("Voltage and Power");
-    }else 
-    alert("Nothing");
+// setting the second menu of (what are we gonna use) based on the first menu (what we want to get .. )
+function bywhat (){
+    if (Get_Menu.value ===Get_Voltage.value){
+        First_Of_By.innerHTML="Power And Resistance";
+        Second_Of_By.innerHTML="Power And Current";
+        Third_Of_By.innerHTML="Current And Resistance";
+    }else if (Get_Menu.value === Get_Current.value){
+        First_Of_By.innerHTML="Power And Resistance";
+        Second_Of_By.innerHTML="Volt And Resistance";  
+        Third_Of_By.innerHTML="Power And Volt";   
+    }else if (Get_Menu.value === Get_Resistance.value){
+        First_Of_By.innerHTML="Volt And Current";
+        Second_Of_By.innerHTML="Power And Current";
+        Third_Of_By.innerHTML="Power And Volt";
+    }else if (Get_Menu.value === Get_Power.value){
+        First_Of_By.innerHTML="Volt And Current";
+        Second_Of_By.innerHTML="Volt And Resistance";
+        Third_Of_By.innerHTML="Current And Resistance";
+    }
 }
 // variable for the whole body
 var body = document.getElementById("body");
 
 // variables for GET menu .. the first menu on the left
+var Get_Menu = document.getElementById("Get");
+Get_Menu.addEventListener("change",bywhat);
 var Get_Voltage = document.getElementById("Voltage");
 var Get_Current = document.getElementById("Current");
 var Get_Resistance = document.getElementById("Resistance"); 
 var Get_Power = document.getElementById("Power");
 
-var menu = document.getElementById("Calculateid").onchange=changeBackGround(this);
-var voltage_And_Current = document.getElementById("voltandcurr");
-var Voltage_And_Power = document.getElementById("Voltandpow");
-var Power_And_Current = document.getElementById("powandcurr");
+//variables for givens .. the second menu on the right
 
+var menu = document.getElementById("Calculateid");
+menu.addEventListener("change",changeBackGround);
+var First_Of_By = document.getElementById("voltandcurr");
+var Second_Of_By = document.getElementById("Voltandpow");
+var Third_Of_By = document.getElementById("powandcurr");
 
+// variables for labels of inputs
 var First_Given = document.getElementById("First_Given");
 var Second_Given = document.getElementById("Second_Given");
 
+// variables for the resutl paragraph tag and result button
 var Result = document.getElementById("Result");
 var Reset = document.getElementById("Reset");
 
-Reset.addEventListener("click",function(){alert("Reset is clicked ");});
+
+Reset.addEventListener("click",ResetingInputs);
+// variables for inputs 
 var Input1 = document.getElementById("Input1");
 var Input2 = document.getElementById("Input2");
-
+Input1.addEventListener("input",Equations);
+Input2.addEventListener("input",Equations);
+function ResetingInputs(){
+    Input1.value = "";
+    Input2.value = ""; 
+}
